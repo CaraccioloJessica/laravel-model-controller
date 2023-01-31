64b0c1ec2716413fb24569a6d1bd64c544b03653
Jessica <jesscara.993@gmail.com>
2023-01-31 17:06:19 +0100

    bonus 2 - footer styled

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,28 @@
 <footer>
-  <div class="background_footer d-flex justify-content-center align-items-center">
-    <h2>Footer section</h2>  
+  <div class="background_footer d-flex justify-content-center">
+    <div class="px-5 py-4">
+      <h3>Contatta supporto</h3>
+      <ul>
+        <li>Lorem ipsum@dolor.sit</li>
+      </ul>
+    </div> 
+
+    <div class="px-5 py-4">
+      <h3>FAQ</h3>
+      <ul>
+        <li>Come aprire un video tramite Apple TV?</li>
+        <li>Come impostare una lingua per la traduzione dei sottotitoli?</li>
+      </ul>
+    </div> 
+
+    <div class="px-5 py-4">
+      <h3>Termini e condizioni</h3>
+      <ul>
+        <li>Copyright</li>
+        <li>Privacy</li>
+        <li>Termini di servizio</li>
+        <li>Termini di pagamento</li>
+      </ul>
+    </div> 
   </div>
 </footer>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <header>
   <div class="d-flex justify-content-center align-items-center my_wrap">
-    <h1>Movies List</h1>
+    <h1>The Movies List</h1>
   </div>
 </header>
